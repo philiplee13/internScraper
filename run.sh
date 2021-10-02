af3e78f66ad06624d065233e01d9acb4ebf19165
philiplee13 <philipyjlee95@gmail.com>
2021-10-01 21:45:26 -0700

    requirements file

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
-echo "Test" &&
+export PATH="/Library/Frameworks/Python.framework/Versions/3.8/bin:/Library/Frameworks/Python.framework/Versions/3.9/bin:/Library/Frameworks/Python.framework/Versions/3.8/bin:/Users/philiplee/opt/anaconda3/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Applications/VMware Fusion.app/Contents/Public:/Library/Frameworks/Python.framework/Versions/3.8/bin:/Library/Frameworks/Python.framework/Versions/3.9/bin:/Users/philiplee/opt/anaconda3/bin" &&
+echo "Running Shell Script for Intern Scraper..." &&
 cd ~/Desktop/Personal_Projects/intern_scraper &&
 source venv/bin/activate &&
 python3 main.py
